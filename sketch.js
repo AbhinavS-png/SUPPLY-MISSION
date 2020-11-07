6b6be361589bcfda9a61a81e9b1bd3fb08f19c60
AbhinavS-png <72965828+AbhinavS-png@users.noreply.github.com>
2020-11-07 15:27:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet;
+var wall;
+
+var speed;
+var weight;
+var thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  
+ speed=random(55,90);
+ weight=random(400,1500);
+ thickness=random(22,83);
+
+ bullet = createSprite(50,200,50,5);
+ bullet.velocityX=speed;
+ bullet.shapeColor=color(225);
+ 
+ wall = createSprite(1400,200,thickness,height/2);
+ wall.shapeColor=color(80,80,80);
+
+}
+
+function draw() {
+  background(0);  
+  if(hasCollided(bullet, wall)){
+    bullet.velocityX=0;
+    var damage=0.5* weight* speed* speed/(thickness *thickness *thickness);
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+      console.log("greater");
+    }
+
+    if(damage<10){
+      wall.shapeColor=color(0,255,0);
+      console.log("smaller");
+    }
+  }
+
+  drawSprites();
+}
+  
+function hasCollided(bullet, wall){
+  bulletRightEdge=bullet.x +bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
